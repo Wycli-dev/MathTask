c47387f52fe49d5d7669eae1e73851a91fbe3e32
Alex Denisov <64966441+Wycli-dev@users.noreply.github.com>
2021-11-21 12:07:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,22 +11,16 @@
 
 using namespace std;
 
-
 int main(int argc, const char * argv[]) {
-
     string source;
 
     cout << "Input your example with spaces like '1 + ( 2 * 3 )'" << endl
          << "=> ";
-
     getline(std::cin, source);
-
     cout << "|" << source << "|" << endl;
 
     MathTask* task = new MathTask(source);
-
     double result = task->get_result();
-
     cout << "result is " << result << endl;
 
     return 0;
